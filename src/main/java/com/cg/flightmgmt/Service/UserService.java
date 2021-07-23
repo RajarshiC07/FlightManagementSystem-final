d7eeb66febb4c44bf295f231bb8d562d31965145
HP <HP@LAPTOP-412H6CRP>
2021-07-23 11:42:49 +0530

    UserService interface

---
+++ src/main/java/com/cg/flightmgmt/Service/UserService.java
@@ -0,0 +1,21 @@
+package com.cg.flightmgmt.Service;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import com.cg.flightmgmt.Entity.Users;
+
+public interface UserService {
+	public Users addUser(Users user);
+	
+	public Users viewUser(BigInteger userId);
+	
+	public List<Users> viewUser();
+	
+	public Users updateUser(Users user);
+	
+	public void deleteUser(BigInteger userId);
+	
+	public void validateUser(Users user); 
+
+}
