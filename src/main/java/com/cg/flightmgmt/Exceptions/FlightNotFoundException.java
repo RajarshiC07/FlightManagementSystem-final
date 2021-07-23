16a5ad0f1c353ee801459edfb3f752a3a4d78d50
USER <USER@JIGSAWsDOCS>
2021-07-23 10:11:46 +0530

    FlightNotFoundException(Exception) Class Commit.

---
+++ src/main/java/com/cg/flightmgmt/Exceptions/FlightNotFoundException.java
@@ -0,0 +1,14 @@
+package com.cg.flightmgmt.Exceptions;
+
+public class FlightNotFoundException extends RuntimeException{
+
+	public FlightNotFoundException(String message)
+	{
+		super(message);
+	}
+	public FlightNotFoundException()
+	{
+		
+	}
+	
+}
