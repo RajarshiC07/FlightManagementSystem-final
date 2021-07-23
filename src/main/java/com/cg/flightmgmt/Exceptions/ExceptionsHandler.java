93160b01b33371e2964420852a576e4b95f4dac7
HP <HP@LAPTOP-412H6CRP>
2021-07-23 10:54:16 +0530

    exception commit

---
+++ src/main/java/com/cg/flightmgmt/Exceptions/ExceptionsHandler.java
@@ -0,0 +1,50 @@
+package com.cg.flightmgmt.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+@ResponseStatus
+public class ExceptionsHandler extends ResponseEntityExceptionHandler {
+	@ExceptionHandler(BookingNotFoundException.class)
+	public ResponseEntity<ErrorMessage> handleBookingNotFoundException(BookingNotFoundException ex,WebRequest req)
+	{
+		ErrorMessage msg=new ErrorMessage(HttpStatus.NOT_FOUND,ex.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(msg);
+	}
+	
+	@ExceptionHandler(FlightNotFoundException.class)
+	public ResponseEntity<ErrorMessage> handleFlightNotFoundException(FlightNotFoundException ex,WebRequest req)
+	{
+		ErrorMessage msg=new ErrorMessage(HttpStatus.NOT_FOUND,ex.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(msg);
+	}
+	
+	@ExceptionHandler(ScheduledFlightNotFoundException.class)
+	public ResponseEntity<ErrorMessage> handleScheduledFlightNotFoundException(ScheduledFlightNotFoundException ex,WebRequest req)
+	{
+		ErrorMessage msg=new ErrorMessage(HttpStatus.NOT_FOUND,ex.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(msg);
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<ErrorMessage> handleUserNotFoundException(UserNotFoundException ex,WebRequest req)
+	{
+		ErrorMessage msg=new ErrorMessage(HttpStatus.NOT_FOUND,ex.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(msg);
+	}
+	
+	@ExceptionHandler(RecordAlreadyPresentException.class)
+	public ResponseEntity<ErrorMessage> handleRecordAlreadyPresentException(RecordAlreadyPresentException ex,WebRequest req)
+	{
+		ErrorMessage msg=new ErrorMessage(HttpStatus.ALREADY_REPORTED,ex.getMessage());
+		return ResponseEntity.status(HttpStatus.ALREADY_REPORTED).body(msg);
+	}
+
+}
