835b8a2abb2f6c87db6c6d1d3dc1ec1765a1b48d
USER <USER@JIGSAWsDOCS>
2021-07-23 11:11:53 +0530

    FlightService (Service Interface) Commit.

---
+++ src/main/java/com/cg/flightmgmt/Service/FlightService.java
@@ -0,0 +1,22 @@
+package com.cg.flightmgmt.Service;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import com.cg.flightmgmt.Entity.Flight;
+
+public interface FlightService {
+	
+	public Flight addFlight(Flight flight); 
+	
+	public  Flight modifyFlight(Flight flight);
+	
+	public Flight viewFlight(BigInteger flightNumber);
+	
+	public List<Flight> viewFlight();
+	
+	public void deleteFlight(BigInteger flightNumber);
+	
+	public void validateFlight(Flight flight);
+	
+}
